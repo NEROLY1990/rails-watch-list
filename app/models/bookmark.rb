7b1763439f6899d44e0e22279ce5686a19457766
LY QUACH <lyllq.quach@gmail.com>
2021-09-25 13:09:41 +0200

    generate models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :List
   belongs_to :Movie
+  validates :comment, presence: true, lenght: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
+
 end
