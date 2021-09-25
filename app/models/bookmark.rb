4bb721f1934f83169cfd9e64ad2d32f1b59e1ef0
LY QUACH <lyllq.quach@gmail.com>
2021-09-25 14:28:34 +0200

    validations ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ApplicationRecord
-  belongs_to :List
-  belongs_to :Movie
-  validates :comment, presence: true, lenght: { minimum: 6 }
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
   validates :movie, uniqueness: { scope: :list }
 
 end
