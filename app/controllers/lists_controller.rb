ee214622b911fdb63d321ea5716cc685af8ea27e
LY QUACH <lyllq.quach@gmail.com>
2021-09-25 16:23:57 +0200

    routage lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+
+  def new
+    @list = List.new(list_params)
+    @list.save
+  end
+  
+  def index
+    @list = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  def show
+    @show = Show.find(param[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #routes lists (index, new, show, create)
+  resources :lists, only: [:create, :index, :show, :new] do
+
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
